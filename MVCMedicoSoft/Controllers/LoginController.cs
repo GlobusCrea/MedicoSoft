bd77b33486889ad0e061f92aa11f40de27addf6a
GlobusCrea <globuscrea@gmail.com>
2014-10-09 16:19:42 +0200

    20141009_16:20

---
+++ MVCMedicoSoft/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMedicoSoft.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]        
+        public ActionResult LoginForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginForm(Utilisateur MyUser)
+        {
+            return View();
+        }
+	}
+}
