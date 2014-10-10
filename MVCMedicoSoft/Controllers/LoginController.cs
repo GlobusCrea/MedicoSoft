e79941bbdfc275f7a30e830bfdad441533abe346
GlobusCrea <globuscrea@gmail.com>
2014-10-10 12:30:52 +0200

    Login

--- MVCMedicoSoft/Controllers/LoginController.cs
+++ MVCMedicoSoft/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using DAL;
+using MVCMedicoSoft.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +17,30 @@ namespace MVCMedicoSoft.Controllers
         }
 
         [HttpPost]
-        public ActionResult LoginForm(Utilisateur MyUser)
+        public ActionResult LoginForm(string txtLogin, string txtPass)
         {
-            return View();
+            Utilisateur u = Utilisateur.AuthentifieMoi(txtLogin, txtPass);
+            if(u == null)
+            {
+                ViewBag.Error = "Try again!";
+                return View();
+            }
+            else
+            {
+                // 1. Stocker en session le login utilisateur
+                MySession.Login = u.Login;
+                MySession.User = u;
+                // 2. Rediriger vers Home/Index (Page d'accueil)
+                return RedirectToRoute(new { Controller="Home", action = "Index" });
+            }
+        }
+
+        [HttpGet]
+        public RedirectToRouteResult LogOut()
+        {
+            MySession.Login = null;
+            Session.Abandon();
+            return RedirectToRoute (new { controller = "Home", action = "Index" });
         }
 	}
 }
---
+++ MVCMedicoSoft/Models/MySession.cs
@@ -0,0 +1,31 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMedicoSoft.Models
+{
+    public static class MySession
+    {
+        public static string Login
+        {
+            get {
+                try {
+                    return HttpContext.Current.Session["login"].ToString();
+                }
+                catch { 
+                    return null; 
+                }
+                
+            }
+            set { HttpContext.Current.Session["login"] = value; }
+        }
+
+        public static Utilisateur User
+        {
+            get { return (Utilisateur)HttpContext.Current.Session["User"]; }
+            set { HttpContext.Current.Session["User"] = value; }
+        }
+    }
+}
