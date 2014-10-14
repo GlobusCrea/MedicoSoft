c8e790eac692a5b61f5fc43475f720433cf7cc2b
GlobusCrea <globuscrea@gmail.com>
2014-10-14 15:03:06 +0200

    Users List

---
+++ MVCMedicoSoft/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using DAL;
+using MVCMedicoSoft.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMedicoSoft.Controllers
+{
+    public class UsersController : Controller
+    {
+        
+        public RedirectToRouteResult Index()
+        {
+            return RedirectToRoute(new { Controller = "Login", Action = "LoginForm" });
+        }
+
+        public ActionResult usersList()
+        {
+            if((MySession.Login == null) && (MySession.Login != null))
+            {
+                return RedirectToRoute(new { Controller = "Login", Action = "LoginForm" });
+            }
+            else
+            {
+                List<Utilisateur> usersList = Utilisateur.getInfos();
+                return View(usersList);
+            }
+        }
+	}
+}
